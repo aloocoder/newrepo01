51ee6f462b850a633555f4b4934a6353c37aacc0
aloocoder <harshw2j2@gmail.com>
2025-05-09 20:17:53 +0530

    reviewd the project structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+## helps in developing our appication as a package
+
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    " this function will return the list of requiremnts"
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+    
+    return requirements
+
+setup(
+    name = 'mlproject01',
+    version = '0.0.1',
+    author = 'harsh',
+    author_email = 'harshw2j2@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt'),
+)
